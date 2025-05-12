dd61c4189c2d0f4c3b0e3963110906e272c0645d
Hansraj Singh Shekhawat <singhhansraj025@gmail.com>
2025-05-12 17:53:34 +0530

    Added renewal and transfer db dump file.

--- LoggingMicroservice/src/config/db.ts
+++ LoggingMicroservice/src/config/db.ts
@@ -4,10 +4,11 @@ import config from "./index";
 
 const connectDB = async () => {
   try {
+    console.log("⚒️ MongoDB connection initiated...");
     await mongoose.connect(config.mongoURI);
-    console.log("MongoDB Connected...");
+    console.log("✅ MongoDB connection successful...");
   } catch (err: any) {
-    console.error("MongoDB Connection Error:", err.message);
+    console.error("❌MongoDB connection unsuccessful...:", err.message);
     // Exit process with failure
     process.exit(1);
   }
